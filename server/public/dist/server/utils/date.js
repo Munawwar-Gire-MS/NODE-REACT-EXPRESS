a9f0b5d5383268ecd8470d8a149e592b7ffc1ebf
munawwar-gire-ms <v-mgire@microsoft.com>
2025-04-20 10:29:45 +0000

    Changes in worker file

--- server/public/dist/server/utils/date.js
+++
@@ -1,6 +0,0 @@
-import { formatInTimeZone } from 'date-fns-tz';
-export function formatDateForClient(date, timeZone) {
-    const dateStr = formatInTimeZone(date, timeZone, 'yyyy-MM-dd');
-    const timeStr = formatInTimeZone(date, timeZone, 'HH:mm');
-    return { date: dateStr, time: timeStr };
-}
